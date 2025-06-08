249b6015e14fd06e42885fdf0f263bd659c9da6d
Elisey Sin <57361980+Nionchik@users.noreply.github.com>
2025-06-08 21:32:04 +0300

    Update MathClient.cpp

--- sem2/EliseySin/mini DZ/1/MathClient/MathClient.cpp
+++ sem2/EliseySin/mini DZ/1/MathClient/MathClient.cpp
@@ -4,11 +4,12 @@
 
 int main() {
     std::vector<double> data = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0 };
+    int size = static_cast<int>(data.size());
 
-    std::cout << "Mean: " << mean(data) << std::endl;
-    std::cout << "Median: " << median(data) << std::endl;
-    std::cout << "Root Mean Square: " << rootMeanSquare(data) << std::endl;
-    std::cout << "Variance: " << variance(data) << std::endl;
+    std::cout << "Mean: " << mean(data.data(), size) << std::endl;
+    std::cout << "Median: " << median(data.data(), size) << std::endl;
+    std::cout << "Root Mean Square: " << rootMeanSquare(data.data(), size) << std::endl;
+    std::cout << "Variance: " << variance(data.data(), size) << std::endl;
 
     return 0;
 }
